8551dffa2d5ae49c3fe5d5a7c60034eb074a0383
Mawcel <theemawcel@gmail.com>
2024-11-03 16:06:08 +0100

    CORS in .env

--- api/index.ts
+++ api/index.ts
@@ -1,22 +1,22 @@
-
-
-const routes = require('./routes')
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 
+const routes = require('./routes')
+
 dotenv.config();
+
 const app = express()
 
 const options: cors.CorsOptions = {
-  origin: ['http://localhost', "http://127.0.0.1:5173"]
+  origin: process.env.CORS?.split(' ')
 };
 
 app.use(cors(options));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use('/', routes)
+app.use('/', routes);
 
 app.listen(process.env.PORT, () => { 
   console.log("Server running at PORT: ", process.env.PORT); 
