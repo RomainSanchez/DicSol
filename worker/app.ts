9ac4fe78d811f4e24bf875fd55de1b12484e6a94
Mawcel <theemawcel@gmail.com>
2024-11-19 20:41:06 +0100

    Add player wallet

--- worker/app.ts
+++ worker/app.ts
@@ -17,6 +17,7 @@ interface Epoch{
 
 interface Player {
   address: string;
+  wallet: string;
   enteredAt: number;
   exitedAt: number;
   oldBalance: number;
@@ -259,6 +260,7 @@ async function getPlayers(epoch: Epoch): Promise<Player[]> {
       const accountData = AccountLayout.decode(account.account.data);
 
       let player: Player = {
+        wallet: account.owner.toString(),
         address: account.pubkey.toString(),
         oldBalance: 0,
         balance: Number(accountData.amount) / (10 ** 9),
