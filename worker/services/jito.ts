ae57b381de4e5b66743c8814f2e854c1cb4a1c9f
Mawcel <theemawcel@gmail.com>
2024-11-24 12:09:29 +0100

    Compoensate for jito update latency

--- worker/services/jito.ts
+++ worker/services/jito.ts
@@ -5,6 +5,9 @@ export class JitoService {
   private readonly baseUrl = 'https://kobe.mainnet.jito.network/api/v1/';
   
   async getRewards(epoch: number): Promise<any> {
+    // Get epoch -2 instead of epoch -1 as jito API doesn't update at the start of the epoch
+    epoch = epoch -1;
+
     try {
       const response = await axios.get<JitoReward[]>(
         `${this.baseUrl}/validators/${process.env.VALIDATOR_ADDRESS!}`
